5f911c1ad09cedf6c738f3b205974609f2469d05
koma3 <limit307703@gmail.com>
2023-11-02 16:31:31 +0900

    タイトル&キャラ調整

---
+++ Assets/UI/TitleSceneMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour
+{
+
+    [SerializeField] AudioClip SE;
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        Invoke("ChangeScene", 1.5f);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.PlayOneShot(SE);
+            Invoke("ChangeScene", 2f);
+        }
+    }
+
+    void ChangeScene()
+    {
+            SceneManager.LoadScene("MenuScene");    //Åu3Åv
+    }
+}
