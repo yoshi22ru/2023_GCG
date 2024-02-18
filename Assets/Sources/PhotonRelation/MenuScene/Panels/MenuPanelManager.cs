e64234410200b60ee8491d463cac7cd6d56440fb
KasuyaShota <22rd055@ms.dendai.ac.jp>
2024-02-18 17:09:21 +0900

    カメラ座標異常修正完了

--- Assets/Sources/PhotonRelation/MenuScene/Panels/MenuPanelManager.cs
+++ Assets/Sources/PhotonRelation/MenuScene/Panels/MenuPanelManager.cs
@@ -20,7 +20,6 @@ public class MenuPanelManager : MonoBehaviourPunCallbacks
     }
 
     public void SetPanel(Ident_Panel ident_Panel) {
-        Debug.Log(ident_Panel);
         now_panel = ident_Panel;
         for (int i = 0; i < panels.Count; ++i) {
             if (i == (int) now_panel) {
