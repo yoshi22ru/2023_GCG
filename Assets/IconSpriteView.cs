5fa01bb74e3a1429663274905cd3a852eb756715
yoshi22ru <22ru020@ms.dendai.ac.jp>
2024-03-14 23:44:38 +0900

    sprite参照修正

--- Assets/IconSpriteView.cs
+++ Assets/IconSpriteView.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using static CharaData;
 using UnityEngine.UI;
+using Resources.Character;
 
 public class IconSpriteView : MonoBehaviour
 {
-    [SerializeField] CharaData charaData;
-    private Sprite charaSprite;
-    public Image charaImage;
+    [SerializeField] CharaDataBase charaData;
+    private Sprite[] charaSprite = new Sprite[9];
+    public Image[] charaImage = new Image[9];
 
     void Start()
     {
-        var data = charaData.CharaSprite;
-        charaSprite = data;
-        charaImage.sprite = charaSprite;
+        for (int i = 0; i < charaImage.Length; i++)
+        {
+            var data = charaData.characterData[i].CharaSprite;
+            charaSprite[i] = data;
+            charaImage[i].sprite = charaSprite[i];
+        }
     }
 }
